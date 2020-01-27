da40b0a1933f4fa08d32207dd0347c2d6e33a850
detaybey <umut@celenli.com>
2020-01-27 10:09:22 +0100

    more removal of unused classes

--- src/frontend/src/components/navigation/Navigation.vue
+++ src/frontend/src/components/navigation/Navigation.vue
@@ -1,11 +1,11 @@
 <template>
-  <div id="nav" class='mb-1'>
+  <div id="nav" class='flex flex-row p-2'>
     <div class="logo">
       <router-link to="/">
       <font-awesome-icon icon="shield-alt" />
       Joseki</router-link>
     </div>
-    <div class="links">
+    <div class="links flex-grow justify-end text-right pt-1">
       <router-link to="/" :active-class="returnClusterOverviewActive()">Cluster Overview</router-link>
       <router-link to="/image-overview" :active-class="returnImageOverviewActive()">Image Overview</router-link>
       <router-link to="/docs">Documentation</router-link>
