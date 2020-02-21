4c86dcbc27abfef1cf15c944dede0c20ac9e4fa8
Igor Kliushnikov <igor.kliushnikov@hotmail.com>
2020-02-21 10:25:26 +0100

    feat(be-db): initial database schema

---
+++ src/backend/joseki.be/joseki.db/entities/IJosekiBaseEntity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace joseki.db.entities
+{
+    /// <summary>
+    /// The base interface for EF core entities.
+    /// </summary>
+    public interface IJosekiBaseEntity
+    {
+        /// <summary>
+        /// Auto-increment PK column.
+        /// </summary>
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Set to current UTC time every time when the record is changed.
+        /// </summary>
+        public DateTime DateUpdated { get; set; }
+
+        /// <summary>
+        /// UTC time, when the record was created.
+        /// </summary>
+        public DateTime DateCreated { get; set; }
+
+        /// <summary>
+        /// Name of the actor who requested the record creation/change.
+        /// </summary>
+        public string ChangedBy { get; set; }
+    }
+}
---
+++ src/backend/joseki.be/joseki.db/entities/ImageScanToCveEntity.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace joseki.db.entities
+{
+    /// <summary>
+    /// Maps exact image scans to list of CVEs in this scan.
+    /// </summary>
+    public class ImageScanToCveEntity : IJosekiBaseEntity
+    {
+        /// <inheritdoc />
+        public int Id { get; set; }
+
+        /// <inheritdoc />
+        public DateTime DateUpdated { get; set; }
+
+        /// <inheritdoc />
+        public DateTime DateCreated { get; set; }
+
+        /// <inheritdoc />
+        public string ChangedBy { get; set; }
+
+        /// <summary>
+        /// Image Scan Result identifier.
+        /// </summary>
+        public int ScanId { get; set; }
+
+        /// <summary>
+        /// CVE identifier.
+        /// </summary>
+        public int CveId { get; set; }
+
+        /// <summary>
+        /// Where the CVE was discovered: container image name or application dependencies file.
+        /// </summary>
+        public string Target { get; set; }
+
+        /// <summary>
+        /// States which exact version of vulnerable package `ScanId` discovered.
+        /// </summary>
+        public string UsedPackageVersion { get; set; }
+
+        /// <summary>
+        /// Reference to associated CVE object.
+        /// </summary>
+        public CveEntity CVE { get; set; }
+
+        /// <summary>
+        /// Reference to Image Scan Result object.
+        /// </summary>
+        public ImageScanResultEntity ImageScan { get; set; }
+    }
+}
