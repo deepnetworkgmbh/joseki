5bbb29e6cf23c06e6591020e3043ffa3a84d0ab4
Igor Kliushnikov <igor.kliushnikov@hotmail.com>
2020-02-27 14:39:21 +0100

    feat(be): added api-versioning

---
+++ src/backend/joseki.be/webapp/Infrastructure/ConfigureSwaggerOptions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace webapp.Infrastructure
+{
+    /// <summary>
+    /// Configures the Swagger generation options.
+    /// </summary>
+    /// <remarks>This allows API versioning to define a Swagger document per API version after the
+    /// <see cref="IApiVersionDescriptionProvider"/> service has been resolved from the service container.</remarks>
+    public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+    {
+        private readonly IApiVersionDescriptionProvider provider;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConfigureSwaggerOptions"/> class.
+        /// </summary>
+        /// <param name="provider">The <see cref="IApiVersionDescriptionProvider">provider</see> used to generate Swagger documents.</param>
+        public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider) => this.provider = provider;
+
+        /// <inheritdoc />
+        public void Configure(SwaggerGenOptions options)
+        {
+            // add a swagger document for each discovered API version
+            foreach (var description in this.provider.ApiVersionDescriptions)
+            {
+                options.SwaggerDoc(description.GroupName, CreateInfoForApiVersion(description));
+            }
+        }
+
+        private static OpenApiInfo CreateInfoForApiVersion(ApiVersionDescription description)
+        {
+            var info = new OpenApiInfo()
+            {
+                Title = "Joseki Backend API",
+                Version = description.ApiVersion.ToString(),
+                Description = "Joseki is a set of services to help you keeping cloud-infrastructure, kubernetes, and docker-images configuration closer to known best-practices",
+                Contact = new OpenApiContact() { Name = "Deep Network GmbH", Email = "info@deepnetwork.com" },
+                License = new OpenApiLicense() { Name = "Apache License 2.0", Url = new Uri("https://github.com/deepnetworkgmbh/joseki/blob/master/LICENSE") },
+            };
+
+            if (description.IsDeprecated)
+            {
+                info.Description += " This API version has been deprecated.";
+            }
+
+            return info;
+        }
+    }
+}
