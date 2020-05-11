6994f828316b531cb3e0c1c1e806b5693e876643
detaybey <umut@celenli.com>
2020-05-11 17:46:03 +0300

    added migration for Ownership

---
+++ src/backend/joseki.be/joseki.db/Migrations/20200511111053_OwnershipTable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace joseki.db.Migrations
+{
+#pragma warning disable SA1413, SA1601, CS1591
+
+    public partial class OwnershipTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ownership",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DateUpdated = table.Column<DateTime>(nullable: false),
+                    DateCreated = table.Column<DateTime>(nullable: false),
+                    ChangedBy = table.Column<string>(nullable: true),
+                    ComponentId = table.Column<string>(nullable: true),
+                    Owner = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ownership", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ownership");
+        }
+    }
+#pragma warning restore SA1413, SA1601, CS1591
+}
