f5ff1639b9af9e670d5318bce9b6022977cf5ee8
Umut Oguz Celenli <umut@celenli.com>
2020-07-22 10:34:12 +0300

    added access control controller and component permissions handler

---
+++ src/backend/joseki.be/webapp/Handlers/ComponentPermissionsHandler.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using joseki.db;
+using joseki.db.entities;
+using Microsoft.EntityFrameworkCore;
+using webapp.Authentication;
+using webapp.Models;
+
+namespace webapp.Handlers
+{
+    /// <summary>
+    /// Returns the list of components joint with the granular user roles.
+    /// </summary>
+    public class ComponentPermissionsHandler
+    {
+        private readonly JosekiDbContext db;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ComponentPermissionsHandler"/> class.
+        /// </summary>
+        /// <param name="db">Joseki database object.</param>
+        public ComponentPermissionsHandler(JosekiDbContext db)
+        {
+            this.db = db;
+        }
+
+        /// <summary>
+        /// Returns list of components with user roles.
+        /// </summary>
+        public async Task<List<ComponentsWithRoles>> GetUserPermissionsOnComponents()
+        {
+            // get list of infrastructure components
+            return await this.db.Set<InfrastructureComponentEntity>()
+                            .AsNoTracking()
+                            .Select(c => new ComponentsWithRoles
+                            {
+                                Id = c.ComponentId,
+                                Name = c.ComponentName,
+                                UserRoles = this.db.ComponentUserRoleRelations
+                                                .Where(x => x.ComponentId == c.ComponentId)
+                                                .Select(r => new UserRolePair
+                                                {
+                                                    UserId = r.UserId,
+                                                    RoleId = r.RoleId,
+                                                }).ToList(),
+                            })
+                            .ToListAsync();
+        }
+
+        /// <summary>
+        /// Updates component-user-role relations.
+        /// </summary>
+        /// <param name="update">List of updated components with new user-role definition.</param>
+        public async Task<bool> SetUserPermissionsOnComponents(List<ComponentsWithRoles> update)
+        {
+            // check for updates and additions
+            foreach (var component in update)
+            {
+                foreach (var userrole in component.UserRoles)
+                {
+                    var record = this.db
+                                     .ComponentUserRoleRelations
+                                     .FirstOrDefault(x => x.ComponentId == component.Id && x.UserId == userrole.UserId);
+
+                    // there was no permission before, add.
+                    if (record == null)
+                    {
+                        this.db.ComponentUserRoleRelations.Add(new ComponentUserRoleRelation
+                        {
+                            ComponentId = component.Id,
+                            UserId = userrole.UserId,
+                            RoleId = userrole.RoleId,
+                        });
+                        await this.db.SaveChangesAsync();
+                    }
+
+                    // this record exists.
+                    else
+                    {
+                        // update if any change occured.
+                        if (record.RoleId != userrole.RoleId)
+                        {
+                            record.RoleId = userrole.RoleId;
+                            await this.db.SaveChangesAsync();
+                        }
+                    }
+                }
+            }
+
+            // check for removals
+            foreach (var record in this.db.ComponentUserRoleRelations)
+            {
+                var componentOnUpdate = update.FirstOrDefault(x => x.Id == record.ComponentId);
+                if (componentOnUpdate == null)
+                {
+                    // TODO: component not available, remove permissions?
+                    continue;
+                }
+
+                var existsOnUpdate = componentOnUpdate.UserRoles.FirstOrDefault(x => x.UserId == record.UserId && x.RoleId == record.RoleId);
+                if (existsOnUpdate == null)
+                {
+                    // the record does not exist on update, remove.
+                    this.db.ComponentUserRoleRelations.Remove(record);
+                    await this.db.SaveChangesAsync();
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/backend/joseki.be/webapp/Models/JosekiUser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace webapp.Authentication
+{
+    /// <summary>
+    /// Active Directory User.
+    /// </summary>
+    public class JosekiUser
+    {
+        /// <summary>
+        /// Id of user.
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Name of user.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// User's joseki app roles.
+        /// </summary>
+        public List<string> AppRoles { get; set; }
+    }
+}
