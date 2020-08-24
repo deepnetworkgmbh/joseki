877d1a01b3fb01615fd162367b35b2c150c21b87
Umut Oguz Celenli <umut@celenli.com>
2020-08-24 10:43:58 +0300

    avoid last scan overflows

--- src/frontend/src/views/Overview.vue
+++ src/frontend/src/views/Overview.vue
@@ -55,7 +55,7 @@
           <table class="w-full text-xs p-4">
             <tbody>
               <tr v-for="(scan,i) in getShortHistory()" :key="`scan${i}`" @click="dayClicked(scan.recordedAt.split('T')[0])" :class='getHistoryClass(scan)'>
-                <td>{{ scan.recordedAt | formatDate }}</td>
+                <td class="w-1">{{ scan.recordedAt | formatDate }}</td>
                 <td class="w-1 text-right">{{scan.score}}%</td>
               </tr>
             </tbody>
